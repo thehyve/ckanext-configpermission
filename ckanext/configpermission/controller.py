443c4f45383bbec7c43f5bb7da863f4aa3f43ea1
Frank Bijlsma <fbijlsma@gmail.com>
2017-06-12 10:58:23 +0200

    Fixing role form collapsing in on itself when moving tr

--- ckanext/configpermission/controller.py
+++ ckanext/configpermission/controller.py
@@ -59,7 +59,7 @@ class PermissionController(BaseController):
 
 def clean_str(string):
     """
-    Tokenization/string cleaning for all datasets except for SST.
+    Tokenization/string cleaning
     Original taken from https://github.com/yoonkim/CNN_sentence/blob/master/process_data.py
     """
     string = re.sub(r"[^A-Za-z0-9(),!?\'\`]", " ", string)
