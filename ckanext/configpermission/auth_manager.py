563e8fb7c4ec90569096efe67711a8fd7f5fa9d6
Frank Bijlsma <fbijlsma@gmail.com>
2017-10-11 10:08:16 +0200

    removing debug

--- ckanext/configpermission/auth_manager.py
+++ ckanext/configpermission/auth_manager.py
@@ -93,8 +93,7 @@ class AuthManager(object):
                 return auth_get._followee_list(context, data_dict)
 
         membership = auth_model.AuthMember.by_group_and_user_id(group_id=owner_org, user_id=user.id)
-        if action == 'view_promoted':
-            import pdb; pdb.set_trace()
+
         # If the user is a member of the org, check if he has the right rank
         if membership is not None:
             if auth.min_role.rank <= membership.role.rank:
