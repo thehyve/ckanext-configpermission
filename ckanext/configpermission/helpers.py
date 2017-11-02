341437e2ae2416b181417a9f087cd279ee5c1e78
Frank Bijlsma <fbijlsma@gmail.com>
2017-11-02 10:39:07 +0100

    fixing helper functions

--- ckanext/configpermission/helpers.py
+++ ckanext/configpermission/helpers.py
@@ -1,8 +1,12 @@
 from ckanext.configpermission.model import AuthMember
-from ckan.logic import check_access as ckan_check_access, NotAuthorized
+from jinja2.runtime import Undefined
 
 
 def get_role(user_id, group_id):
+    if type(user_id) == Undefined:
+        user_id = None
+    if type(group_id) == Undefined:
+        group_id = None
     member = AuthMember.by_group_and_user_id(group_id=group_id, user_id=user_id)
     if member is None or member.role is None:
         return 'N/A'
@@ -10,5 +14,13 @@ def get_role(user_id, group_id):
 
 
 def get_role_selected(user_id, group_id):
+    if type(user_id) == Undefined:
+        user_id = None
+    if type(group_id) == Undefined:
+        group_id = None
+
     member = AuthMember.by_group_and_user_id(group_id=group_id, user_id=user_id)
-    return member.role.name
+    if member is None or member.role is None:
+        return ''
+    else:
+        return member.role.name
