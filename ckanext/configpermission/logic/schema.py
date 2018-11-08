5e830e3a28cf525fc4947ee1299c382e5dfcb363
Joris Borgdorff <joris@thehyve.nl>
2018-11-08 13:40:23 +0100

    Try to fix invite in organization

--- ckanext/configpermission/logic/schema.py
+++ ckanext/configpermission/logic/schema.py
@@ -1,11 +1,11 @@
 from ckanext.configpermission.logic.validators import role_exists
-from ckan.logic.validators import group_id_exists, user_name_exists
+from ckan.logic.validators import not_missing, group_id_or_name_exists, user_id_or_name_exists
 
 
 def member_schema():
     schema = {
-        'id': [group_id_exists, unicode],
-        'username': [user_name_exists, unicode],
-        'role': [role_exists, unicode],
+        'id': [not_missing, group_id_or_name_exists, unicode],
+        'username': [not_missing, user_id_or_name_exists, unicode],
+        'role': [not_missing, role_exists, unicode],
     }
     return schema
